50c60f1687a6271c93ff1e5fb12a753953e1aac2
monognuisy <ysmsmart1@gmail.com>
2024-04-13 22:00:29 +0900

    feat: 임시 uuid로 유저 요청 처리, 프로세스 풀 사용

--- app/util/convert.py
+++ app/util/convert.py
@@ -1,14 +1,15 @@
 import os
+import asyncio
+import subprocess
 from pydub import AudioSegment
 
 def midi_to_mp3(midi_file, soundfont, mp3_file):
     # Convert MIDI to WAV using fluidsynth
     wav_file = mp3_file.replace('.mp3', '.wav')
     print(wav_file)
-    os.system(f'fluidsynth -ni {soundfont} {midi_file} -F {wav_file} -r 44100')
-    
-    # Convert WAV to MP3 using pydub
-    audio = AudioSegment.from_wav(wav_file)
-    audio.export(mp3_file, format='mp3')
-    # Remove temporary WAV file
-    os.remove(wav_file)
+
+    subprocess.run(
+        f'fluidsynth -ni {soundfont} {midi_file} -F - -r 44100 -o audio.file.type=au -q | ffmpeg -i - -b:a 192K {mp3_file}',
+        shell=True,
+        check=True,
+    )
