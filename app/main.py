953ba84f23c4261c739a1624c6541057cee6a2c9
monognuisy <ysmsmart1@gmail.com>
2024-07-07 11:59:03 +0900

    fix: ffmpeg doens't end

--- app/main.py
+++ app/main.py
@@ -33,9 +33,6 @@ logger.addHandler(ch)
 assets_dir_path = './app/assets/'
 music_dir_path = assets_dir_path + 'music/'
 
-def log_info(req_body, res_body):
-    logger.info(req_body)
-
 async def delete_music_file(music_uuid: str):
     global music_dir_path
 
@@ -49,22 +46,11 @@ async def delete_music_file(music_uuid: str):
         os.remove(midi_file)
         os.remove(mp3_file)
         print(f'deleted {music_uuid}.mid, {music_uuid}.mp3')
-    except:
+    except Exception as e:
+        logger.error(e)
+        logger.info(traceback.format_exc())
+
         print('failed to remove')
-
-@app.middleware('http')
-async def some_middleware(request: Request, call_next):
-    req_body = await request.body()
-    logger.info(req_body)
-
-    response = await call_next(request)
-
-    res_body = b''
-    async for chunk in response.body_iterator:
-        res_body += chunk
-
-    return Response(content=res_body, status_code=response.status_code,
-                    headers=dict(response.headers), media_type=response.media_type)
 
 @app.post('/music', status_code=200)
 async def get_music(music_body: MusicBody, background_tasks: BackgroundTasks):
@@ -100,10 +86,13 @@ async def get_music(music_body: MusicBody, background_tasks: BackgroundTasks):
     try:
         loop = asyncio.get_event_loop()
         await loop.run_in_executor(executor, convert.midi_to_mp3, midi_file, soundfont_path, mp3_file)
-    except:
+    except Exception as e:
         header['isSuccess'] = 'false'
         header['code'] = '500'
         header['message'] = 'music rendering fail'
+
+        logger.error(e)
+        logger.info(traceback.format_exc())
 
         # 비어있는 파일을 반환
         return JSONResponse('', headers=header)
