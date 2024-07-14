2a380714e379a0ca1bc320455fc5fe04336afa12
monognuisy <ysmsmart1@gmail.com>
2024-07-14 11:31:13 +0900

    test: for performance

--- app/main.py
+++ app/main.py
@@ -22,84 +22,17 @@ class MusicBody(BaseModel):
 
 app = FastAPI()
 
-executor = ThreadPoolExecutor(max_workers=1)
-
-logger = logging.getLogger("main")
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setFormatter(CustomFormatter())
-logger.addHandler(ch)
-
 assets_dir_path = './app/assets/'
 music_dir_path = assets_dir_path + 'music/'
 
-async def delete_music_file(music_uuid: str):
-    global music_dir_path
-
-    midi_file = music_dir_path + f'{music_uuid}.mid'
-    mp3_file = music_dir_path + f'{music_uuid}.mp3'
-
-    timeout = 100
-
-    try:
-        await asyncio.sleep(timeout)
-        os.remove(midi_file)
-        os.remove(mp3_file)
-        print(f'deleted {music_uuid}.mid, {music_uuid}.mp3')
-    except Exception as e:
-        logger.error(e)
-        logger.info(traceback.format_exc())
-
-        print('failed to remove')
-
 @app.post('/music', status_code=200)
-async def get_music(music_body: MusicBody, background_tasks: BackgroundTasks):
-    uuid_prefix = str(uuid.uuid1())
-    midi_file = music_dir_path + f'{uuid_prefix}.mid'
-    mp3_file = music_dir_path + f'{uuid_prefix}.mp3'
-    soundfont_path = assets_dir_path + 'soundfont.sf2'
-
+async def get_music(music_body: MusicBody):
     header = {
         'isSuccess': 'true',
         'code': '200',
         'message': '',
     }
 
-    try:
-        generator.make_song(
-            genre=music_body.genre,
-            mood=music_body.mood,
-            tempo=music_body.tempo,
-            music_path=midi_file,
-        )
-    except Exception as e:
-        header['isSuccess'] = 'false'
-        header['code'] = '500'
-        header['message'] = 'music generation fail'
-
-        logger.error(e)
-        logger.info(traceback.format_exc())
-
-        # 비어있는 파일을 반환
-        return JSONResponse('', headers=header)
-    
-    try:
-        future = executor.submit(convert.midi_to_mp3, midi_file, soundfont_path, mp3_file)
-        future.result()
-
-    except Exception as e:
-        header['isSuccess'] = 'false'
-        header['code'] = '500'
-        header['message'] = 'music rendering fail'
-
-        logger.error(e)
-        logger.info(traceback.format_exc())
-
-        # 비어있는 파일을 반환
-        return JSONResponse('', headers=header)
-
-    header['message'] = 'music generation success'
-
-    background_tasks.add_task(delete_music_file, uuid_prefix)
+    mp3_file = music_dir_path + 'sample.mp3'
 
     return FileResponse(mp3_file, headers=header)
